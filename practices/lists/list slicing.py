95006bd2018f066d36331ebb568c5cac89f68c65
Ditsa Rakshit <ditsarakshit20@gmail.com>
2025-07-10 12:11:33 +0530

    added lists

---
+++ practices/lists/list slicing.py
@@ -0,0 +1,10 @@
+s = [[0, 2], [4, 6], [8, 10], [12, 14]]
+print(s[0])
+print(s[3][1])
+p = ["chair", "table", "desk", "lamp", "bed"]
+print(p[-5])
+print("most people own at least" + " " + str(s[0][1]) + " " + p[0] + "s")
+q = [0.98, 8.76, 6.54, 4.32]
+print(q[1:])
+print(q[1:3])
+print(q[:2])
---
+++ practices/lists/lists_in&not_in.py
@@ -0,0 +1,14 @@
+
+l1 = [4, 5.67, True, "i am a girl", [3, 78, 29]]
+l2 = list("memymine")
+print("e" in l2)
+print("a" not in l2)
+
+
+
+
+
+
+
+
+
